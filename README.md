589a0194ca11a1c3fa931d144d71aa473643b852
ShyamSundarB <shyambaskaran@outlook.com>
2021-07-22 19:12:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+![Vue Storefront](https://camo.githubusercontent.com/48c886ac0703e3a46bc0ec963e20f126337229fc/68747470733a2f2f643968687267346d6e767a6f772e636c6f756466726f6e742e6e65742f7777772e76756573746f726566726f6e742e696f2f32383062313964302d6c6f676f2d76735f3062793032633062793032633030303030302e6a7067)
+
+# Boilerplate for Vue Storefront 2 eCommerce integration
+
+<a href="https://discord.vuestorefront.io">![Our Discord server](https://img.shields.io/discord/770285988244750366?label=join%20discord&logo=Discord&logoColor=white)</a>
+
+This repository is a starting point for creating new integrations for [Vue Storefront 2](https://github.com/vuestorefront/vue-storefront).
+
+To learn how to build your integration, see our [Integration guide](https://docs.vuestorefront.io/v2/integrate/integration-guide.html).
---
+++ packages/theme/nuxt.config.js
@@ -0,0 +1,138 @@
+import webpack from 'webpack';
+
+export default {
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Vue Storefront',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'crossorigin'
+      },
+      {
+        rel: 'preload',
+        href: 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap',
+        as: 'style'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap',
+        media: 'print',
+        onload: 'this.media=\'all\'',
+        once: true
+      }
+    ]
+  },
+
+  loading: { color: '#fff' },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // to core
+    '@nuxt/typescript-build',
+    '@nuxtjs/style-resources',
+    ['@vue-storefront/nuxt', {
+      // @core-development-only-start
+      coreDevelopment: true,
+      // @core-development-only-end
+      useRawSource: {
+        dev: [
+          '@vue-storefront/boilerplate',
+          '@vue-storefront/core'
+        ],
+        prod: [
+          '@vue-storefront/boilerplate',
+          '@vue-storefront/core'
+        ]
+      }
+    }],
+    // @core-development-only-start
+    ['@vue-storefront/nuxt-theme', {
+      generate: {
+        replace: {
+          apiClient: '@vue-storefront/boilerplate-api',
+          composables: '@vue-storefront/boilerplate'
+        }
+      }
+    }],
+    // @core-development-only-end
+    /* project-only-start
+    ['@vue-storefront/nuxt-theme'],
+    project-only-end */
+    ['@vue-storefront/boilerplate/nuxt', {}]
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    'nuxt-i18n',
+    'cookie-universal-nuxt',
+    'vue-scrollto/nuxt',
+    '@vue-storefront/middleware/nuxt'
+  ],
+
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: {
+          welcome: 'Welcome 1'
+        },
+        de: {
+          welcome: 'Welcome 2'
+        }
+      }
+    }
+  },
+
+  styleResources: {
+    scss: [require.resolve('@storefront-ui/shared/styles/_helpers.scss', { paths: [process.cwd()] })]
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [
+      'vee-validate/dist/rules'
+    ],
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.VERSION': JSON.stringify({
+          // eslint-disable-next-line global-require
+          version: require('./package.json').version,
+          lastCommit: process.env.LAST_COMMIT || ''
+        })
+      })
+    ]
+  },
+
+  router: {
+    scrollBehavior (_to, _from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else {
+        return { x: 0, y: 0 };
+      }
+    }
+  }
+};
